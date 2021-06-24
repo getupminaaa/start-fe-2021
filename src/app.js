e4860915c9e087505e53065166993a0fd959e2aa
getupminaaa <getupmina0405@gmail.com>
2021-06-24 09:51:17 +0900

    quiz.json 데이터 가져옴

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const $classTable = document.querySelector("#class");
-const $classBadge = document.querySelector("#badge");
+const $quizTable = document.querySelector("#quiz");
+
 fetch("./class.json")
   .then(function (response) {
     return response.json();
@@ -18,18 +19,33 @@ fetch("./class.json")
         <tr>
           <th scope="row">${i + 1}</th>
           <td>${classData[i]["title"]}</td>
-          <td>
-              <a href="${
-                classData[i]["docUrl"]
-              } class="badge bg-secondary">문서</a>
-          </td>
+          <td><a href="${
+            classData[i]["docUrl"]
+          } class="badge bg-secondary">문서</a></td>
           <td> ${links}</td>
           <td>${classData[i]["date"]}</td>
-          <td>
-              <a href="${classData[i]["gitUrl"]}">git</a>
-          </td>
+          <td><a href="${classData[i]["gitUrl"]}">git</a></td>
         </tr>
       `;
       $classTable.innerHTML = str;
+    }
+  });
+
+fetch("./quiz.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (quizData) {
+    let str = "";
+    for (let i = 0; i < quizData.length; i = i + 1) {
+      str += `
+        <tr>
+          <td>${quizData[i]["title"]}</td>
+          <td><a class="badge bg-secondary"href="${quizData[i].docUrl}">문서</a></td>
+          <td><a href="${quizData[i]["previewUrl"]}">보기</a></td>
+          <td><a href="${quizData[i]["gitUrl"]}">git</a></td>
+        </tr>
+      `;
+      $quizTable.innerHTML = str;
     }
   });
