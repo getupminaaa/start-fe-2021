3043c66333441c9ee31a14d81c33cfe2d4f945da
getupminaaa <getupmina0405@gmail.com>
2021-06-24 23:53:03 +0900

    배포

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "none",
+  mode: "development",
   entry: "./src/app.js",
   output: {
     filename: "app.js",
